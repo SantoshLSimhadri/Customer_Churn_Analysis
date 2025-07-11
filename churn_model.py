b94ac65d51da05a13084003f509d040819f6af8a
LeelaSantosh_Simhadri <santoshchowdarysimhadri.23f@gmail.com>
2025-07-11 10:47:30 -0400

    Delete churn_model.py

--- churn_model.py
+++
@@ -1,9 +0,0 @@
-# Python Example: Basic churn model
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-
-df = pd.read_csv('data/customer_data.csv')
-X = df[['tenure', 'monthly_charges']]
-y = df['churn']
-model = LogisticRegression().fit(X, y)
-print(f'Model accuracy: {model.score(X, y):.2f}')
